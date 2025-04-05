5454795dfbfe808dd6ec614c1c30b77621da6f4e
Marcus Vinicius <111466820+MarcusSouza02@users.noreply.github.com>
2025-04-04 21:46:45 -0300

    Delete PCMania directory

--- PCMania/src/MemoriaUSB.java
+++
@@ -1,9 +0,0 @@
-class MemoriaUSB {
-    String nome;
-    int capacidade;
-
-    public MemoriaUSB(String nome, int capacidade) {
-        this.nome = nome;
-        this.capacidade = capacidade;
-    }
-}
--- PCMania/src/READ.md
+++
@@ -1,3 +0,0 @@
-Nome: Marcus Vinicius Faria de Souza   
-Curso: Engenharia de Software  
-Matricula: 354
